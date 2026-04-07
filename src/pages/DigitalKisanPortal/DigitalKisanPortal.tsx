70823759a40e865b0df366c79f8ea0b24c6ba81e
kaushik-ak <kaushik.aditya.ak47@gmail.com>
2026-04-07 06:51:45 +0530

    Create DigitalKisanPortal.tsx

---
+++ src/pages/DigitalKisanPortal/DigitalKisanPortal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./DigitalKisanPortal.module.css";
+
+const DigitalKisanPortal = () => {
+  return (
+    <div className={styles.container || ""}>
+      <h1>Digital Kisan Portal 🌾</h1>
+      <p>Welcome to your agriculture AI platform</p>
+    </div>
+  );
+};
+
+export default DigitalKisanPortal;
